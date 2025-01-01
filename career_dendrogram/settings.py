52543554a157a7b72b7cd01feb275dbba063ebae
Prathamesh Kasar <prathameshkasar000@gmail.com>
2025-01-01 08:11:21 +0530

    Fix static file URL configuration in settings.py
    - Verified and ensured STATIC_URL is correctly set to '/static/' in settings.py for proper static file handling.

--- career_dendrogram/settings.py
+++ career_dendrogram/settings.py
@@ -98,7 +98,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Directories to look for static files
 STATICFILES_DIRS = [
